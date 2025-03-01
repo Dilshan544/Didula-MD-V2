4e2001b6d389992600e358d4c7b05e9c018fdd10
Dilshan544 <ashinsadilshan64@gmail.com>
2025-03-02 01:24:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID === undefined ? 'PRABATH-MD~WDwxiJCB#_TeMYVHC-MVW5wNW-xp9SSsLNTbwLUFdYtCleYo3Msg' : process.env.SESSION_ID,
+    SESSION_ID: process.env.SESSION_ID === undefined ? 'PRABATH-MD~rCZG1RTQ#t40Z0ANrepkXOPP-R2oTlVN2_Q_r9XQD9I4O0R4iBwo' : process.env.SESSION_ID,
     PREFIX: process.env.PREFIX || '.',
     FOOTER: process.env.FOOTER || "*✦𝐃𝐢𝐝𝐮𝐥𝐚 𝐌𝐃 𝐕𝟐✦*",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/za6ytm.jpg",
